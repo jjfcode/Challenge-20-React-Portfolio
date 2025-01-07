5ee327d4e7cf08c19a592166b1ed759117510099
John Ferlito <johnferlito73@gmail.com>
2025-01-07 18:39:44 -0500

    fix about me

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 function Navbar({ handlePageChange, currentPage }) {
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-            <a class="navbar-brand" href="#">Professional Portfolio</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <a className="navbar-brand" href="#">Professional Portfolio</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link nav-link-large" href="#about" onclick="loadSection('about.html')">About Me</a></li>
-                    <li class="nav-item"><a class="nav-link nav-link-large" href="#portfolio" onclick="loadSection('portfolio.html')">Portfolio</a></li>
-                    <li class="nav-item"><a class="nav-link nav-link-large" href="#contact" onclick="loadSection('contact.html')">Contact</a></li>
-                    <li class="nav-item"><a class="nav-link nav-link-large" href="#resume" onclick="loadSection('resume.html')">Resume</a></li>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item"><a className="nav-link nav-link-large" href="#" onClick={e=>handlePageChange("About")}>About Me</a></li>
+                    <li className="nav-item"><a className="nav-link nav-link-large" href="#" onclick="loadSection('portfolio.html')">Portfolio</a></li>
+                    <li className="nav-item"><a className="nav-link nav-link-large" href="#" onclick="loadSection('contact.html')">Contact</a></li>
+                    <li className="nav-item"><a className="nav-link nav-link-large" href="#" onclick="loadSection('resume.html')">Resume</a></li>
                 </ul>
             </div>
         </nav>
