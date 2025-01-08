7ca655be1590e339d3608bee9c345c2930f9bea9
John Ferlito <johnferlito73@gmail.com>
2025-01-07 20:24:36 -0500

    add resume

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -11,9 +11,9 @@ function Navbar({ handlePageChange, currentPage }) {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item"><a className="nav-link nav-link-large" href="#" onClick={e=>handlePageChange("About")}>About Me</a></li>
-                    <li className="nav-item"><a className="nav-link nav-link-large" href="#" onclick={e=>handlePageChange("Portfolio")}>Portfolio</a></li>
-                    <li className="nav-item"><a className="nav-link nav-link-large" href="#" onclick={e=>handlePageChange("Contact")}>Contact</a></li>
-                    <li className="nav-item"><a className="nav-link nav-link-large" href="#" onclick={e=>handlePageChange("Resume")}>Resume</a></li>
+                    <li className="nav-item"><a className="nav-link nav-link-large" href="#" onClick={e=>handlePageChange("Portfolio")}>Portfolio</a></li>
+                    <li className="nav-item"><a className="nav-link nav-link-large" href="#" onClick={e=>handlePageChange("Contact")}>Contact</a></li>
+                    <li className="nav-item"><a className="nav-link nav-link-large" href="#" onClick={e=>handlePageChange("Resume")}>Resume</a></li>
                 </ul>
             </div>
         </nav>
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,6 +1,39 @@
 function Resume() {
   return (
-<h1>Resume</h1>
+    <section id="resume" classNameName="container my-5">
+      <h2>Resume</h2>
+      <a href="file/RESUME-JJF.pdf" className="btn btn-primary my-3" download>Download Resume</a>
+      <h3>Technologies</h3>
+      <div className="row">
+        <div className="col-md-6">
+          <h4>Front-End</h4>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>PWA</li>
+            <li>MERN</li>
+          </ul>
+        </div>
+        <div className="col-md-6">
+          <h4>Back-End</h4>
+          <ul>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>Python</li>
+            <li>Django</li>
+            <li>SQL</li>
+            <li>NoSQL</li>
+            <li>Git</li>
+            <li>Web API</li>
+            <li>ORM</li>
+            <li>MVC</li>
+          </ul>
+        </div>
+      </div>
+    </section>
   );
 }
 
